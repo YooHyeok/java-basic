54b5b94e3aab6d0b86b6a989b7bee0f44d224ee8
YooHyeok <yjk7454@naver.com>
2025-08-02 20:46:24 +0900

    feat: `클래스와 데이터 문제풀이` - 1. 영화리뷰 관람하기1

---
+++ src/class1/ex/MovieReviewMain.java
@@ -0,0 +1,19 @@
+package class1.ex;
+
+public class MovieReviewMain {
+
+    public static void main(String[] args) {
+
+        MovieReview mr1 = new MovieReview();
+        mr1.title = "인셉션";
+        mr1.review = "인생은 무한루프";
+
+        MovieReview mr2 = new MovieReview();
+        mr2.title = "어바웃 타임";
+        mr2.review = "인생 시간 영화!";
+
+        System.out.println("영화 제목: " + mr1.title + ", 리뷰: " + mr1.review);
+        System.out.println("영화 제목: " + mr2.title + ", 리뷰: " + mr2.review);
+
+    }
+}
