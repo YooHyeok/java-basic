9c121eaf6476e798162ed5d26a15c27d514e2e06
YooHyeok <yjk7454@naver.com>
2025-08-10 20:32:17 +0900

    chore: 섹션 03 기본형과 참조형 패키지 구조 변경

---
+++ src/섹션03_기본형과참조형/파트14_기본형vs참조형2_변수대입/Data.java
@@ -0,0 +1,5 @@
+package 섹션03_기본형과참조형.파트14_기본형vs참조형2_변수대입;
+
+public class Data {
+    int value;
+}
--- src/섹션03_기본형과참조형/파트14_기본형vs참조형2_변수대입/VarChange2.java
+++ src/섹션03_기본형과참조형/파트14_기본형vs참조형2_변수대입/VarChange2.java
@@ -1,4 +1,4 @@
-package ref;
+package 섹션03_기본형과참조형.파트14_기본형vs참조형2_변수대입;
 
 public class VarChange2 {
     public static void main(String[] args) {
